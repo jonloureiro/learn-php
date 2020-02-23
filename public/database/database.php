4eca39fcb87485d7524b003525e385bd66f427e0
jonathan.loureiro.m@gmail.com <me@jonloureiro.dev>
2020-02-23 06:35:06 -0400

    corrigindo msg no echo

--- public/database/database.php
+++ public/database/database.php
@@ -7,7 +7,7 @@ $sql = 'CREATE DATABASE IF NOT EXISTS learning_php';
 $result = $connection->query($sql);
 
 if ($result) {
-    echo "Sucesso: " . $result;
+    echo "Database iniciado" . "<br>";
 } else {
     echo "Erro: " . $connection->error;
 }
