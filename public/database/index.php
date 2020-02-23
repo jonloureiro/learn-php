1e01cdd7f7da5a0fcca9cb074b70458e0afdf2bb
jonathan.loureiro.m@gmail.com <me@jonloureiro.dev>
2020-02-23 07:11:56 -0400

    melhorando visualização da página

--- public/database/index.php
+++ public/database/index.php
@@ -1,16 +1,42 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
+
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <link rel="stylesheet" href="../assets/styles/sanitize.css">
+  <link rel="stylesheet" href="../assets/styles/typography.css">
+  <link rel="stylesheet" href="../assets/styles/forms.css">
+
+  <style>
+    :root {
+      --main-txt-color: #353535;
+      --main-bg-color: #F1F2EA;
+    }
+    
+    html {
+      font-size: 200%;
+    }
+
+    body {
+      padding: 1rem;
+      background-color: var(--main-bg-color);
+      color: var(--main-txt-color);
+    }
+  </style>
+
   <title>PHP + MySQL</title>
 </head>
-<body>
-    <?php
-        require_once 'database.php';
-        require_once 'create_table.php';
 
-        echo "<br><br>" . "Tudo certo!";
-    ?>
+<body>
+  <?php
+      require_once 'database.php';
+      require_once 'create_table.php';
+
+      echo "<br><br>" . "Tudo certo!";
+  ?>
 </body>
+
 </html>
